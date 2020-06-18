41fa4a901fb0387caf7c16165dbe4998e90b6f56
lja781 <lja781@student.bham.ac.uk>
2020-06-18 18:37:38 +0100

    Added login capability

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import login as func_login
 from django.contrib.auth import authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import logout as func_logout
 from django.shortcuts import render, redirect
 
@@ -8,7 +8,20 @@ def login(request):
     meta = {"is_logged_in":request.user.is_authenticated}
     if request.user.is_authenticated:
         return redirect('profile')
-    return render(request, 'accounts/login.html', {'meta':meta})
+    elif request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=raw_password)
+            if user is not None:
+                func_login(request, user)
+                return redirect('home')
+            else:
+                pass
+                #return redirect("")
+    form = AuthenticationForm(request)
+    return render(request, 'accounts/login.html', {'form':form, 'meta':meta})
 
 def signup(request):
     meta = {"is_logged_in":request.user.is_authenticated}
@@ -33,7 +46,7 @@ def profile(request):
     return render(request, 'accounts/profile.html', {'meta':meta, 'user':user})
 
 def logout(request):
-    meta = {"is_logged_in":request.user.is_authenticated}
+    meta = {"is_logged_in":False}
     if request.user.is_authenticated:
         func_logout(request)
     else:
